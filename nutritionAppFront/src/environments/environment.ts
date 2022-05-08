1ae5d6ba4ffbab5196c965bad3c1096e9b443ea0
Malek Douik <mohamedmalek.douik@gmail.com>
2022-05-08 17:48:42 +0100

    1st commit 08/05/2022

--- nutritionAppFront/src/environments/environment.ts
+++ nutritionAppFront/src/environments/environment.ts
@@ -1,8 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-/*
 export const environment = {
   production: false,
   "utilisateur_url": "https://localhost:44343/api/Utilisateur",
@@ -16,9 +11,9 @@ export const environment = {
   "rendezvous_url":"https://localhost:44343/api/Rendezvous",
   "contrainteAlim_url":"https://localhost:44343/api/ContrainteNutritionnelle"
 };
-*/
 
 
+/*
 export const environment = {
   production: false,
   "utilisateur_url": "./api/Utilisateur",
@@ -32,13 +27,6 @@ export const environment = {
   "rendezvous_url":"./api/Rendezvous",
   "contrainteAlim_url":"./api/ContrainteNutritionnelle"
 };
+*/
 
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
